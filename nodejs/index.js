8a2df50836b352c5765fe5b46d4ec5225fa61dee
akamtt <theusluiz0@gmail.com>
2022-02-18 10:34:51 -0300

    Initial commit

---
+++ nodejs/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(helmet());
+app.use(() => morgan('tiny'));
+app.use(() => cors()); 
